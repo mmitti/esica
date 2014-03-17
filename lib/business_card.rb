f9e08bd055ecfef3325200061493480cef88f613
hirano <yprrrrr.mmmmkp@gmail.com>
2014-03-17 14:25:48 +0900

    delete unimportant class

--- lib/business_card.rb
+++ lib/business_card.rb
@@ -42,13 +42,6 @@ class BusinessCard
     end
   end
 
-  class RequiredText < Text
-    def initialize(*args)
-      super
-      raise ArgumentError, "missing keyword: text" if @text.nil?
-    end
-  end
-
   class Image < Base
     def initialize(
       x:      self.class.const_get(:X),
@@ -106,49 +99,49 @@ class BusinessCard
     end
   end
 
-  class Family < RequiredText
+  class Family < Text
     X         = 180
     Y         = 220
     FONT_SIZE = 72
   end
 
-  class Name < RequiredText
+  class Name < Text
     X         = 380
     Y         = 220
     FONT_SIZE = 72
   end
 
-  class RubiFamily < RequiredText
+  class RubiFamily < Text
     X         = 220
     Y         = 150
     FONT_SIZE = 25
   end
 
-  class RubiName < RequiredText
+  class RubiName < Text
     X         = 400
     Y         = 150
     FONT_SIZE = 25
   end
 
-  class School < RequiredText
+  class School < Text
     X         = 220
     Y         = 50
     FONT_SIZE = 25
   end
 
-  class Department < RequiredText
+  class Department < Text
     X         = 300
     Y         = 90
     FONT_SIZE = 25
   end
 
-  class Mail < RequiredText
+  class Mail < Text
     X         = 220
     Y         = 380
     FONT_SIZE = 20
   end
 
-  class Tel < RequiredText
+  class Tel < Text
     X         = 220
     Y         = 350
     FONT_SIZE = 20
