d043b0eb29896b1c7611b7121053285a49c0e5ba
aokabin <mi.aoka.breakdance@gmail.com>
2014-03-20 00:18:24 +0900

    add file of png

--- sansan.rb
+++ sansan.rb
@@ -95,8 +95,12 @@ class Esica < Sinatra::Base
     end
     halt 400 unless validate(parameters)
 
+
+    f.write(parameters["family"])
+
     pic  = tempfile ["pic", ".png"], parameters["pic"]
     back = tempfile ["back", ".png"], parameters["back"]
+    f.write(parameters["school"])
 
     business_card = BusinessCard.new(
       name:        BusinessCard::Name.new(text: parameters["name"]),
